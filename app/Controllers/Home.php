e367290dd6094b6613b945a555436c02e1a34a64
maliks1 <maliksyafii426@gmail.com>
2023-10-30 17:15:36 +0700

    can connect to sql

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -11,13 +11,12 @@ class Home extends BaseController
 
     public function about(): string
     {
-        dd($this->mahasiswaModel->getAllMahasiswa());
+        // dd($this->mahasiswaModel->getAllMahasiswa());
         $mahasiswa = $this->mahasiswaModel->getAllMahasiswa();
         $data = [
             "title" => "Home",
             "mahasiswa" => $mahasiswa
         ];
-        var_dump($data);
         return view('index', $data);
         // return view('index');
     }
